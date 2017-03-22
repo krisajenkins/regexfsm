f97c831e94979dd9a31a793832961459faa16718
Kris Jenkins <krisajenkins@gmail.com>
2017-03-22 09:57:36 +0000

    A bit of refactoring.

--- src/Main.purs
+++ src/Main.purs
@@ -1,14 +1,14 @@
 module Main where
 
-import Data.Foldable (foldl)
-import Data.Map (Map, lookup)
+import Prelude
 import Data.Map as Map
+import Data.Set as Set
+import Data.Foldable (class Foldable, foldl)
+import Data.Map (Map, lookup)
 import Data.Maybe (Maybe, maybe)
 import Data.Set (Set)
-import Data.Set as Set
 import Data.String (toCharArray)
 import Data.Tuple (Tuple(..))
-import Prelude
 
 data Node a
   = Start
@@ -33,45 +33,66 @@ instance eqNode :: Eq a => Eq (Node a) where
 instance showNode :: Show a => Show (Node a) where
   show Start = "Start"
   show End = "End"
-  show (Leaf a) = "Leaf " <> show a
+  show (Leaf a) = "(Leaf " <> show a <> ")"
 
 type Regex = Map (Node Int) (Map Char (Set (Node Int)))
-
--- | a*b
-regex1 :: Regex
-regex1 =
-  Map.fromFoldable
-  [ Tuple Start (Map.fromFoldable [ Tuple 'a' (Set.singleton Start), Tuple 'b' (Set.singleton End) ])
-  ]
-
--- | (ab|ac)+
-regex2 :: Regex
-regex2 =
-  Map.fromFoldable
-  [ Tuple Start (Map.fromFoldable [ Tuple 'a' (Set.fromFoldable [ Leaf 1, Leaf 2 ]) ])
-  , Tuple (Leaf 1) (Map.fromFoldable [ Tuple 'b' (Set.fromFoldable [ Start, End ]) ])
-  , Tuple (Leaf 2) (Map.fromFoldable [ Tuple 'c' (Set.fromFoldable [ Start, End ]) ])
-  ]
 
 match :: Regex -> String -> Boolean
 match regex string =
   Set.member End lastHeads
   where
     lastHeads :: Set (Node Int)
-    lastHeads = foldl (step regex) (Set.singleton Start) (toCharArray string)
+    lastHeads = foldl (stepHeads regex) (Set.singleton Start) (toCharArray string)
 
-
-step :: Regex -> Set (Node Int) -> Char -> Set (Node Int)
-step regex heads current =
+-- | Given the current character in the string we're matching, stepHeads
+-- | each head (each pointer into the graph) forward.
+stepHeads :: Regex -> Set (Node Int) -> Char -> Set (Node Int)
+stepHeads regex heads current =
   foldl reducer Set.empty heads
   where
     reducer :: Set (Node Int) -> (Node Int) -> Set (Node Int)
-    reducer accum head = Set.union accum (singleHead regex current head)
+    reducer accum head = Set.union accum $ stepHead regex current head
+
+
+stepHead :: Regex -> Char -> Node Int -> Set (Node Int)
+stepHead regex current head = withDefault Set.empty $ do
+  links <- lookup head regex
+  lookup current links
 
 withDefault :: forall a. a -> Maybe a -> a
 withDefault x = maybe x id
 
-singleHead :: Regex -> Char -> Node Int -> Set (Node Int)
-singleHead regex current head = withDefault Set.empty $ do
-  links <- lookup head regex
-  lookup current links
+------------------------------------------------------------
+-- Sample regex state machines.
+
+-- | a*b
+regex1 :: Regex
+regex1 =
+  Map.fromFoldable
+  [ Start -=> [ 'a' =-> [ Start ]
+              , 'b' =-> [ End ] ]
+  ]
+
+
+-- | (ab|ac)+
+regex2 :: Regex
+regex2 =
+  Map.fromFoldable
+  [ Start -=> [ 'a' =-> [ Leaf 1, Leaf 2 ] ]
+  , Leaf 1 -=> [ 'b' =-> [ Start, End ] ]
+  , Leaf 2 -=> [ 'c' =-> [ Start, End ] ]
+  ]
+
+------------------------------------------------------------
+-- Helpers for constructing graphs by-hand.
+
+nodeToEdge :: forall h t c f.
+  (Ord c , Foldable f) => h -> f (Tuple c t) -> Tuple h (Map c t)
+nodeToEdge head tail = Tuple head (Map.fromFoldable tail)
+
+edgeToNodes :: forall h t f.
+  (Foldable f, Ord t) => h -> f t -> Tuple h (Set t)
+edgeToNodes head tail = Tuple head (Set.fromFoldable tail)
+
+infixl 9 nodeToEdge as -=>
+infixl 9 edgeToNodes as =->
